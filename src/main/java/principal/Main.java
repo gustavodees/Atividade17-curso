d6389f53d5ad61a7f23584542e11ae6ae65b2beb
ChocolateKest <gustavoemartins@gmail.com>
2025-06-21 23:25:21 -0300

    First commit

---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,12 @@
+package principal;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double preco = 34.5;
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println("Desconto: " + desconto);
+
+    }
+}
